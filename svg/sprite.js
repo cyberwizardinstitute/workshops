d910c91aec6ae9ed46afa48d25bf96ecfb287346
James Halliday <mail@substack.net>
2015-01-22 14:53:54 -0800

    sprite example

---
+++ svg/sprite.js
@@ -0,0 +1,22 @@
+var sprite = require('box-sprite-svg');
+var player = sprite(document.querySelector('svg #player'));
+
+var loop = require('frame-loop');
+var engine = loop(function (dt) {
+    player.tick(dt);
+});
+engine.run();
+
+window.addEventListener('keydown', function (ev) {
+    if (ev.keyCode === 37) {
+        player.velocity.x = -400;
+    }
+    else if (ev.keyCode === 39) {
+        player.velocity.x = 400;
+    }
+});
+window.addEventListener('keyup', function (ev) {
+    if (ev.keyCode === 37 || ev.keyCode === 39) {
+        player.velocity.x = 0;
+    }
+});
