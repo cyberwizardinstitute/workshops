2b2e12ba658b31f0498ee0b97e6f547b3fc8c7bc
substack <substack@9nTgtYmvW4HID6ayt6Icwc8WZxdifx5SlSKKIX/X/1g=.ed25519>
2017-06-09 13:20:10 +0200

    updated network server

--- networks/server.js
+++ networks/server.js
@@ -1,15 +1,11 @@
-var ecstatic = require('ecstatic');
-var http = require('http');
-var parseform = require('body/any');
-var st = ecstatic(__dirname + '/static');
+var http = require('http')
+var parseform = require('body/any')
 
 var server = http.createServer(function (req, res) {
-    if (req.method === 'POST') {
-        parseform(req, res, function (err, params) {
-            console.log(params);
-            res.end('ok\n');
-        });
-    }
-    else st(req, res)
-});
-server.listen(5000);
+  console.log(req.method, req.url, req.headers)
+  parseform(req, res, function (err, params) {
+    console.log(params)
+    res.end('ok\n')
+  })
+})
+server.listen(5000)
